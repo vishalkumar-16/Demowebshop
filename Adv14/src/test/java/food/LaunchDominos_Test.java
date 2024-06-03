5a20e3d3f28d17e26f2e19ed2e6b784503fabf1b
vishalkumar-16 <vishalkumar827001@gmail.com>
2024-06-03 17:23:55 +0530

    second commit

--- Adv14/src/test/java/food/LaunchDominos_Test.java
+++ Adv14/src/test/java/food/LaunchDominos_Test.java
@@ -1,23 +1,23 @@
-package food;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
-
-public class LaunchDominos_Test {
-	
-	@Test
-	public void dominos() throws InterruptedException {
-		WebDriver driver = new FirefoxDriver();
-		driver.manage().window().maximize();
-		String url = System.getProperty("url");
-		driver.get(url);
-		
-		//driver.get("https://pizzaonline.dominos.co.in/");
-		Thread.sleep(2000);
-		driver.manage().window().minimize();
-		driver.quit();
-	}
-
-}
+//package food;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.annotations.Test;
+//
+//public class LaunchDominos_Test {
+//	
+//	@Test
+//	public void dominos() throws InterruptedException {
+//		WebDriver driver = new FirefoxDriver();
+//		driver.manage().window().maximize();
+//		String url = System.getProperty("url");
+//		driver.get(url);
+//		
+//		//driver.get("https://pizzaonline.dominos.co.in/");
+//		Thread.sleep(2000);
+//		driver.manage().window().minimize();
+//		driver.quit();
+//	}
+//
+//}
